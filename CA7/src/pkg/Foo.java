83aa3b002bfdc1d05cd2e71ff50d853ac823fa5c
Amanda Mitchell <amandavsu2018@gmail.com>
2018-03-23 15:33:39 -0400

    added pre-created classes

---
+++ CA7/src/pkg/Foo.java
@@ -0,0 +1,17 @@
+package pkg;
+
+public class Foo {
+	private int bit=0;
+	public Foo() {
+	}
+	public int getBits() {
+		return bit;
+	}
+	public void increment() {
+		bit++;
+	}
+	@Override
+	public String toString() {
+		return "bit=" + bit;
+	}
+}
